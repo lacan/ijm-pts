cbe2153bb9cc3ded7d92e1c4c7b5f389c7db7d42
Olivier Burri <olivier.burri@epfl.ch>
2016-05-19 14:50:05 +0200

    Jar Modified

--- updateJar.bat
+++ updateJar.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 
 ECHO Copying BIOP_Pizza.ijm to current folder
-copy C:\Fiji.app\plugins\ActionBar\BIOP_Pizza.ijm D:\Projects\Fiji\biop-pizza-target
+copy C:\Fiji\plugins\ActionBar\BIOP_Pizza.ijm D:\Projects\Fiji\biop-pizza-target
 
 ECHO Creating JAR FILE
 jar cf BIOP_PTS.jar plugins.config *.ijm
 
 ECHO Copying BIOP_PTS.jar to Fiji folder
-copy BIOP_PTS.jar C:\Fiji.app\plugins\BIOP
+copy BIOP_PTS.jar C:\Fiji\plugins\BIOP
 
 PAUSE
